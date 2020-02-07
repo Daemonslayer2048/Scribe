c4d70d308864bab2483ada9d240d1ea6d14f534a
Daemonslayer2048 <jhanafin@desktop.hanafin.lan>
2020-02-06 23:07:25 -0600

    Update DB

--- SQL_Scripts/db_setup.sql
+++ SQL_Scripts/db_setup.sql
@@ -1,38 +1,69 @@
 PRAGMA foreign_keys = 1;
 
-create table users (
+CREATE TABLE users (
   'pk' INTEGER PRIMARY KEY,
-  'username' text NOT NULL UNIQUE
+  'username' TEXT NOT NULL UNIQUE,
+  'group' INTEGER,
+  CONSTRAINT fk_group
+    FOREIGN KEY ('group')
+    REFERENCES groups ('pk')
 );
 
-create table device_models (
+CREATE TABLE groups (
   'pk' INTEGER PRIMARY KEY,
-  'manufacturer' text NOT NULL,
-  'model' text NOT NULL,
-  'OS' text
+  'groupname' TEXT NOT NULL UNIQUE
 );
 
-create table devices (
+CREATE TABLE device_models (
   'pk' INTEGER PRIMARY KEY,
-  'ip' text NOT NULL,
+  'manufacturer' TEXT NOT NULL,
+  'model' TEXT NOT NULL,
+  'OS' TEXT
+);
+
+CREATE TABLE repos (
+	'pk' INTEGER PRIMARY KEY NOT NULL,
+	'name' TEXT NOT NULL
+);
+
+CREATE TABLE remote_repos (
+	'pk' INTEGER PRIMARY KEY NOT NULL,
+	'name' TEXT NOT NULL,
+	'URL' TEXT NOT NULL,
+  	CONSTRAINT fk_name
+  	  FOREIGN KEY ('name')
+  	  REFERENCES repos ('name')
+);
+
+CREATE TABLE proxies (
+	'pk' INTEGER PRIMARY KEY NOT NULL,
+	'ip' TEXT NOT NULL,
+  	'port' INT DEFAULT 22,
+  	'username' TEXT NOT NULL,
+    'password' TEXT NOT NULL
+);
+
+CREATE TABLE devices (
+  'pk' INTEGER PRIMARY KEY,
+  'ip' TEXT NOT NULL,
   'port' INT DEFAULT 22,
-  'alias' text,
-  'model' text NOT NULL,
-  'user' text,
-  'username' text NOT NULL,
-  'password' text NOT NULL,
-  'enable' text,
-  'last_updated' text DEFAULT "Never",
-  'enabled' text DEFAULT "True",
+  'alias' TEXT,
+  'model' TEXT NOT NULL,
+  'user' TEXT,
+  'username' TEXT NOT NULL,
+  'password' TEXT NOT NULL,
+  'enable' TEXT,
+  'last_updated' TEXT NOT NULL DEFAULT "Never",
+  'enabled' TEXT NOT NULL DEFAULT "True",
+  'repo' TEXT NOT NULL DEFAULT 1,
+  'proxy' INTEGER,
   CONSTRAINT fk_models
     FOREIGN KEY ('model')
-    REFERENCES device_models ('pk')
-);
-
-create table config (
-
-);
-
-create table proxies (
-
+    REFERENCES device_models ('pk'),
+  CONSTRAINT fk_repo
+  	FOREIGN KEY ('repo')
+  	REFERENCES repos ('pk'),
+  CONSTRAINT fk_proxy
+    FOREIGN KEY ('proxy')
+    REFERENCES proxies ('pk')
 );
