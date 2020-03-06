3649d60812054bacf8a91c80b45d0b6927b682cb
Daemonslayer2048 <jhanafin@desktop.hanafin.lan>
2020-03-05 20:57:01 -0600

    Need to stash

--- Scribe/web/__init__.py
+++ Scribe/web/__init__.py
@@ -1,10 +1,14 @@
 import connexion
 from flask import Flask
 from Scribe.config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = connexion.FlaskApp(__name__, specification_dir="../api")
 app.add_api("swagger.yaml")
 app = app.app
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-from . import routes
+from . import routes, models
